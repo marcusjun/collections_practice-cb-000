b77434128fdee0e8a252addd9287258d9cf0df7d
Marcus Jun <mjun@calstate.edu>
2018-04-07 17:31:08 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  describe '#swap_elements_from_to' do
+    it 'swap the index and destination_index elements of an array' do
+      expect(swap_elements_from_to(["a","b","c"]0,2)).to eq(["c", "b", "a"])
+      expect(swap_elements_from_to(["a","b","c"]2,1)).to eq(["a", "c", "b"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
