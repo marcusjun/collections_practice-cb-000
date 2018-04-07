bd7b2ab0c9f8b8c0a474426a9e3832200a2ba23c
Marcus Jun <mjun@calstate.edu>
2018-04-07 17:31:34 +0000

    automatically backed up by learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -32,8 +32,8 @@ describe 'collections practice' do
 
   describe '#swap_elements_from_to' do
     it 'swap the index and destination_index elements of an array' do
-      expect(swap_elements_from_to(["a","b","c"]0,2)).to eq(["c", "b", "a"])
-      expect(swap_elements_from_to(["a","b","c"]2,1)).to eq(["a", "c", "b"])
+      expect(swap_elements_from_to(["a","b","c"],0,2)).to eq(["c", "b", "a"])
+      expect(swap_elements_from_to(["a","b","c"],2,1)).to eq(["a", "c", "b"])
     end
   end
 
